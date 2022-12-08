6cbcbd5a6db9fe8aae9d29846ea5e1ed1910cdcc
Joao <j.bittencourt1@outlook.com>
2022-12-08 09:23:06 -0300

    Adicionadas mais funcionalidades pro chatbot

--- helpers/messagesBot.js
+++ helpers/messagesBot.js
@@ -10,6 +10,18 @@ const messages = [
     {
         send: "#linkedin",
         response: "https://www.linkedin.com/company/saltsystems/"
+    },
+    {
+        send: "#portfolio",
+        response: "https://portfolio-joao-bittencourt.vercel.app/"
+    },
+    {
+        send: "#github",
+        response: "https://github.com/Joaob1"
+    },
+    {
+        send: "#criador",
+        response: "O meu criador foi João Bittencourt. Ele mora em Salvador - BA e é apaixonado por tecnologia. Confira o github dele com o comando '#github', ou o portfolio com o comando '#portfolio'. Nós agradecemos a sua presença aqui! :)"
     }
 ]
 export default messages;
